ca37add50fca2560f70dcc36d36466476689aa17
Matt Nearents <nearents.matt@gmail.com>
2018-11-05 17:09:42 -0800

    css and js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
+//var $cell = $('.card');
+//
+////open and close card when clicked on card
+//$cell.find('.js-expander').click(function() {
+//
+//  var $thisCell = $(this).closest('.card');
+//
+//  if ($thisCell.hasClass('is-collapsed')) {
+//    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+//    $thisCell.removeClass('is-collapsed').addClass('is-expanded');
+//    
+//    if ($cell.not($thisCell).hasClass('is-inactive')) {
+//      //do nothing
+//    } else {
+//      $cell.not($thisCell).addClass('is-inactive');
+//    }
+//
+//  } else {
+//    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+//    $cell.not($thisCell).removeClass('is-inactive');
+//  }
+//});
+//
+////close card when click on cross
+//$cell.find('.js-collapser').click(function() {
+//
+//  var $thisCell = $(this).closest('.card');
+//
+//  $thisCell.removeClass('is-expanded').addClass('is-collapsed');
+//  $cell.not($thisCell).removeClass('is-inactive');
+//
+//});
+
+
 var $cell = $('.card');
 
 //open and close card when clicked on card
@@ -6,18 +40,14 @@ $cell.find('.js-expander').click(function() {
   var $thisCell = $(this).closest('.card');
 
   if ($thisCell.hasClass('is-collapsed')) {
-    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
+    $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed');
     $thisCell.removeClass('is-collapsed').addClass('is-expanded');
     
-    if ($cell.not($thisCell).hasClass('is-inactive')) {
-      //do nothing
-    } else {
-      $cell.not($thisCell).addClass('is-inactive');
-    }
+    
 
   } else {
     $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-    $cell.not($thisCell).removeClass('is-inactive');
+    $cell.not($thisCell);
   }
 });
 
@@ -27,6 +57,6 @@ $cell.find('.js-collapser').click(function() {
   var $thisCell = $(this).closest('.card');
 
   $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-  $cell.not($thisCell).removeClass('is-inactive');
+  $cell.not($thisCell);
 
 });
